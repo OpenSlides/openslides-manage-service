bc9f6f12e3878d964f012804f5fc6eb130498dc4
Oskar Hahn <mail@oshahn.de>
2021-03-13 08:22:49 +0100

    hash password

--- pkg/server/reset_password.go
+++ pkg/server/reset_password.go
@@ -1,1 +1,47 @@
 package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+const (
+	authURL  = "http://auth:9004"
+	hashPath = "/internal/auth/hash"
+)
+
+func hashPassword(ctx context.Context, password string) (string, error) {
+	reqBody := fmt.Sprintf(`{"toHash": "%s"}`, password)
+	req, err := http.NewRequestWithContext(ctx, "POST", authURL+hashPath, strings.NewReader(reqBody))
+	if err != nil {
+		return "", fmt.Errorf("creating request to auth service: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("sending request: %w", err)
+	}
+
+	if resp.StatusCode != 200 {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			body = []byte("[can not read body]")
+		}
+		return "", fmt.Errorf("auth service returned %s: %s", resp.Status, body)
+	}
+
+	hash, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("reading body: %w", err)
+	}
+	return string(hash), nil
+}
+
+func setPassword(ctx context.Context, userID int, hash string) error {
+	return errors.New("TODO")
+}
