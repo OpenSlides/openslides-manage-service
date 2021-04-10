6bdd7cad8d315aaf608ae3eb03826fcc733e077e
Norman Jäckel <openslides@normanjaeckel.de>
2021-04-11 01:21:58 +0200

    Added func createSecret.

--- pkg/manage/cmd_start.go
+++ pkg/manage/cmd_start.go
@@ -3,6 +3,8 @@ package manage
 import (
 	"context"
 	"fmt"
+	"io/fs"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -32,8 +34,39 @@ func CmdStart(cfg *ClientConfig) *cobra.Command {
 			return fmt.Errorf("creating Docker Compose YML: %w", err)
 		}
 
+		if err := createSecrets(); err != nil {
+			return fmt.Errorf("creating secrets: %w", err)
+		}
+
 		return nil
 	}
 
 	return cmd
+}
+
+// create Secrets creates random values uses as secrets in Docker Compose file.
+func createSecrets() error {
+	path := "secrets/"
+	if err := os.MkdirAll(path, fs.ModePerm); err != nil {
+		return fmt.Errorf("creating directory `%s`: %w", path, err)
+	}
+
+	secrets := []string{
+		"auth_token_key",
+		"auth_cookie_key",
+	}
+	for _, s := range secrets {
+		f, err := os.Create(path + s)
+		if err != nil {
+			return fmt.Errorf("creating file `%s`: %w", path+s, err)
+		}
+		defer f.Close()
+
+		r := "my random value" // TODO: Use a random value here.
+		if _, err := f.WriteString(r); err != nil {
+			return fmt.Errorf("writing secret to file `%s`: %w", f.Name(), err)
+		}
+	}
+
+	return nil
 }
