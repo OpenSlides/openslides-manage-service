f6acc84b144f9ac4a72fc7e0aaff9a4bc1f60cba
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-06-12 10:56:41 +0200

    Adjust Workflows #2

--- dev/run-tests.sh
+++ dev/run-tests.sh
@@ -3,14 +3,14 @@
 # Executes all tests. Should errors occur, CATCH will be set to 1, causing an erronous exit code.
 
 echo "########################################################################"
-echo "###################### Start full system tests #########################"
+echo "###################### Run Tests and Linters ###########################"
 echo "########################################################################"
 
 IMAGE_TAG=openslides-manage-tests
 CATCH=0
 PERSIST_CONTAINERS=$1
 
-make build-test || CATCH=1
+if [ "$(docker images -q $IMAGE_TAG)" = "" ]; then make build-test || CATCH=1; fi
 docker run ${IMAGE_TAG} || CATCH=1
 
 if [ -z $PERSIST_CONTAINERS ]; then docker stop $(docker ps -a -q --filter ancestor=${IMAGE_TAG} --format="{{.ID}}") || CATCH=1; fi
