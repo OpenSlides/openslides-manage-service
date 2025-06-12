641524715c48e34ef0e516cd98e25abe9ff223bf
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-06-12 14:28:59 +0200

    Adjust Workflows #3

--- dev/command.sh
+++ dev/command.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-if [ ! -z $dev   ]; then CompileDaemon -log-prefix=false -build="go build ./cmd/server" -command="./server"; fi
+if [ ! -z $dev   ]; CompileDaemon -log-prefix=false -build="go build ./cmd/server" -command="./server"; fi
 if [ ! -z $tests ]; then make test; fi
