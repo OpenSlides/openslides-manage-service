81a7c74453e451adc35ee1fe72dc77f886ff5d77
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-06-24 13:41:35 +0200

    Lint shell-scripts using Shellcheck #2

--- dev/run-tests.sh
+++ dev/run-tests.sh
@@ -17,6 +17,6 @@ CATCH=0
 if [ "$(docker images -q $IMAGE_TAG)" = "" ]; then make build-test || CATCH=1; fi
 docker run ${IMAGE_TAG} || CATCH=1
 
-if [ -z "$PERSIST_CONTAINERS" ]; then docker stop "$(docker ps -a -q --filter ancestor=${IMAGE_TAG} --format="{{.ID}}")" || CATCH=1; fi
+if [ -z "$PERSIST_CONTAINERS" ]; then docker stop $(docker ps -a -q --filter ancestor=${IMAGE_TAG} --format="{{.ID}}") || CATCH=1; fi
 
 exit $CATCH
