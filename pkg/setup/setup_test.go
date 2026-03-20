1433bef1bd8a115bb01ea84d92021f4ad7f70e0b
Jan Malte Behrens <janmtbehrens@gmail.com>
2026-03-20 10:12:02 +0100

    Changing version 15 to 17

--- pkg/setup/setup_test.go
+++ pkg/setup/setup_test.go
@@ -311,7 +311,7 @@ services:
 		testFileContains(t, testDir, myFileName, "image: example.com/test_Waetai0ohf/openslides-proxy:2.0.0")
 		testFileContains(t, testDir, myFileName, "image: example.com/test_Waetai0ohf/openslides-client:latest")
 		testFileContains(t, testDir, myFileName, "ports:\n      - 127.0.0.1:8001:8000")
-		testFileContains(t, testDir, myFileName, "image: postgres:15")
+		testFileContains(t, testDir, myFileName, "image: postgres:17")
 		testFileNotContains(t, testDir, myFileName, "cert_crt")
 		testKeyFile(t, secDir, "auth_token_key")
 		testKeyFile(t, secDir, "auth_cookie_key")
@@ -331,7 +331,7 @@ disablePostgres: true
 		if err := setup.Setup(testDir, false, nil, c); err != nil {
 			t.Fatalf("running Setup() failed with error: %v", err)
 		}
-		testFileNotContains(t, testDir, myFileName, "image: postgres:15")
+		testFileNotContains(t, testDir, myFileName, "image: postgres:17")
 	})
 
 	t.Run("running setup.Setup() and create all stuff in tmp directory using yet another custom config", func(t *testing.T) {
@@ -615,7 +615,7 @@ services:
       - superadmin
 
   postgres:
-    image: postgres:15
+    image: postgres:17
     environment:
       << : *default-environment
       POSTGRES_DB: openslides
